a1d73a9bcf3c919f6282a35b89d83051ee29a919
michal.kohut <michalkoh@gmail.com>
2023-05-10 22:47:29 +0200

    Building up...

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,12 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Different Experience</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>Enjoy a New Modern Fire</p>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                    <li><a href="#one" className="button next scrolly">Browse</a></li>
                 </ul>
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
+import logo from '../assets/images/logo.png'
+
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></Link>
+        <Link to="/" className="logo">
+            <strong>FIRE TABLE</strong>
+        </Link>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
