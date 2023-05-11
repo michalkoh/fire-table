ec07daf0895c63b6eba4b0304e3c2679deb23e0d
michal <michalkoh@gmail.com>
2023-05-11 09:16:32 +0200

    Icon added.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Forty V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Forty by HTML5 UP"
+    title: "Fire Table",
+    author: "Michal Kohut",
+    description: "Portfolio Website for the Fire Tables."
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -15,7 +15,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        icon: 'src/assets/images/web-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
