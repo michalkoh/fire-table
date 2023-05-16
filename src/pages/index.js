c0d7d9f62fbd9ea0c2ebea3f169340e51698ab0b
michal.kohut <michalkoh@gmail.com>
2023-05-16 22:54:02 +0200

    image-bkg package installed

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,36 +6,33 @@ import BannerMajor from '../components/BannerMajor'
 import pic01 from '../assets/images/pic01.jpg'
 import pic02 from '../assets/images/pic02.jpg'
 
-class HomeIndex extends React.Component {
-    render() {
+const HomeIndex = () =>  {
+    return (
+        <Layout>
 
-        return (
-            <Layout>
+            <BannerMajor />
 
-                <BannerMajor />
+            <div id="main">
+                <section id="one" className="tiles">
+                    <article style={{backgroundImage: `url(${pic01})`}}>
+                        <header className="major">
+                            <h3>Portfolio</h3>
+                            <p>Discover our work</p>
+                        </header>
+                        <Link to="/portfolio" className="link primary"></Link>
+                    </article>
+                    <article style={{backgroundImage: `url(${pic02})`}}>
+                        <header className="major">
+                            <h3>About us</h3>
+                            <p>Who we are and what we are doing</p>
+                        </header>
+                        <Link to="/about" className="link primary"></Link>
+                    </article>
+                </section>
+            </div>
 
-                <div id="main">
-                    <section id="one" className="tiles">
-                        <article style={{backgroundImage: `url(${pic01})`}}>
-                            <header className="major">
-                                <h3>Portfolio</h3>
-                                <p>Discover our work</p>
-                            </header>
-                            <Link to="/portfolio" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic02})`}}>
-                            <header className="major">
-                                <h3>About us</h3>
-                                <p>Who we are and what we are doing</p>
-                            </header>
-                            <Link to="/about" className="link primary"></Link>
-                        </article>
-                    </section>
-                </div>
-
-            </Layout>
-        )
-    }
+        </Layout>
+    )
 }
 
 export const Head = () => (
