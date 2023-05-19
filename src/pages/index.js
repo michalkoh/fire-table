886d53878106d651788c233f91e2031997befcb5
michal.kohut <michalkoh@gmail.com>
2023-05-19 07:24:23 +0200

    Styling gatsby images

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,9 +34,7 @@ const HomeIndex = () =>  {
                                 style={{
                                     position: "unset",
                                     overflow: "auto",
-                                gridArea: "1/1",
-                                // You can set a maximum height for the image, if you wish.
-                                //maxHeight: 600,
+                                    gridArea: "1/1"
                                 }}
                                 //layout="fullWidth"
                                 // You can optionally force an aspect ratio for the generated image
@@ -57,8 +55,47 @@ const HomeIndex = () =>  {
                                 }}
                             >
                                 {/* Any content here will be centered in the component */}
-                                <h1>Hero text</h1>
+                                <header className="major">
+                                    <h3>About us</h3>
+                                    <p>Who we are and what we are doing</p>
+                                </header>
                             </div>
+                            <Link  to="/about" className="link primary"></Link>
+                    </article>
+
+                    <article className='bkg-image-grid'>
+                            {/* You can use a GatsbyImage component if the image is dynamic */}
+                            <StaticImage
+                                style={{
+                                    position: "unset",
+                                    overflow: "auto",
+                                    gridArea: "1/1"
+                                }}
+                                //layout="fullWidth"
+                                // You can optionally force an aspect ratio for the generated image
+                                // This is a presentational image, so the alt should be an empty string
+                                alt=""
+                                // Assisi, Perúgia, Itália by Bernardo Ferrari, via Unsplash
+                                src="../assets/images/pic01.jpg" 
+                                placeholder="blurred"
+                            />
+                            <div
+                                style={{
+                                // By using the same grid area for both, they are stacked on top of each other
+                                gridArea: "1/1",
+                                position: "relative",
+                                // This centers the other elements inside the hero component
+                                placeItems: "center",
+                                //display: "grid",
+                                }}
+                            >
+                                {/* Any content here will be centered in the component */}
+                                <header className="major">
+                                    <h3>About us</h3>
+                                    <p>Who we are and what we are doing</p>
+                                </header>
+                            </div>
+                            <Link  to="/about" className="link primary"></Link>
                     </article>
                 </section>
             </div>
