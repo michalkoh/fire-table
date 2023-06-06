b0e5eb39c596be44264c8a9bf4660bb083e188e5
michal <michalkoh@gmail.com>
2023-06-06 07:41:19 +0200

    Instagram link added.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                {/* <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li> */}
+                <li><a href="https://www.instagram.com/fire__table_" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                 <li><a href="https://www.youtube.com/channel/UCEri2ZRgjddzJBxfYIQApfQ" className="icon alt fa-youtube"><span className="label">YouTube</span></a></li>
             </ul>
             <ul className="copyright">
